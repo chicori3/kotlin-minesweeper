0e2a105ae437d21356105d2b91aa2e2744fac52b
sian <chicori3@naver.com>
2024-12-24 23:52:57 +0900

    refactor: 방향 탐색 시 0 이하의 값이 올 수 있기 때문에 제거

--- src/main/kotlin/cell/CoordinateValue.kt
+++ src/main/kotlin/cell/CoordinateValue.kt
@@ -2,10 +2,6 @@ package cell
 
 @JvmInline
 value class CoordinateValue(val value: Int) {
-    init {
-        require(value >= 0) { "좌표 값은 0 이상이어야 합니다." }
-    }
-
     operator fun plus(other: CoordinateValue): CoordinateValue =
         CoordinateValue(value + other.value)
 
