f7e302f8541cf18a4f1592271c8db67bc7dbda42
sian <chicori3@naver.com>
2025-01-02 18:10:06 +0900

    feat: Cell 프로퍼티 추가

--- src/main/kotlin/cell/Cell.kt
+++ src/main/kotlin/cell/Cell.kt
@@ -1,7 +1,35 @@
 package cell
 
-sealed interface Cell
+sealed interface Cell {
+    val revealed: Boolean
+    fun reveal(): Cell
+    fun shouldFloodFill(): Boolean
+}
 
-data class BlankCell(val adjacentMineCount: MineCount) : Cell
+data class BlankCell(
+    val adjacentMineCount: MineCount,
+    private var _revealed: Boolean = false
+) : Cell {
+    override val revealed: Boolean
+        get() = _revealed
 
-data object MineCell : Cell
+    override fun reveal(): Cell {
+        _revealed = true
+        return this
+    }
+
+    override fun shouldFloodFill(): Boolean = adjacentMineCount == MineCount.ZERO
+}
+
+data object MineCell : Cell {
+    private var _revealed: Boolean = false
+    override val revealed: Boolean
+        get() = _revealed
+
+    override fun reveal(): Cell {
+        _revealed = true
+        return this
+    }
+
+    override fun shouldFloodFill(): Boolean = false
+}
